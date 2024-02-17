56f39261089e2b42e8d3a6b061793de34d5975c8
Hancie123 <hanciewanemphago@gmail.com>
2024-02-17 23:50:01 +0545

    login ui added

---
+++ public/assets/js/main.js
@@ -0,0 +1,302 @@
+
+
+var themeOptionArr = {
+			typography: '',
+			version: '',
+			layout: '',
+			primary: '',
+			headerBg: '',
+			navheaderBg: '',
+			sidebarBg: '',
+			sidebarStyle: '',
+			sidebarPosition: '',
+			headerPosition: '',
+			containerLayout: '',
+			direction: '',
+		};
+ 	
+
+(function($) {
+	
+	"use strict"
+	
+	var direction =  getUrlParams('dir');
+	var theme =  getUrlParams('theme');
+	
+	/* Dz Theme Demo Settings  */
+	
+	var dezThemeSet0 = { /* Default Theme */
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		headerBg: "color_1",
+		primary: "color_1",
+		navheaderBg: "color_1",
+		sidebarBg: "color_1",
+		sidebarStyle: "full",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	var dezThemeSet1 = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		primary: "color_15",
+		headerBg: "color_1",
+		navheaderBg: "color_13",
+		sidebarBg: "color_13",
+		sidebarStyle: "full",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	var dezThemeSet2 = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		primary: "color_7",
+		headerBg: "color_1",
+		navheaderBg: "color_7",
+		sidebarBg: "color_1",
+		sidebarStyle: "modern",
+		sidebarPosition: "static",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	
+	var dezThemeSet3 = {
+		typography: "poppins",
+		version: "light",
+		layout: "horizontal",
+		primary: "color_3",
+		headerBg: "color_1",
+		navheaderBg: "color_1",
+		sidebarBg: "color_3",
+		sidebarStyle: "full",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	var dezThemeSet4 = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		primary: "color_9",
+		headerBg: "color_9",
+		navheaderBg: "color_9",
+		sidebarBg: "color_1",
+		sidebarStyle: "compact",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	var dezThemeSet5 = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		primary: "color_7",
+		headerBg: "color_1",
+		navheaderBg: "color_7",
+		sidebarBg: "color_7",
+		sidebarStyle: "icon-hover",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	var dezThemeSet6 = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		primary: "color_3",
+		headerBg: "color_3",
+		navheaderBg: "color_1",
+		sidebarBg: "color_1",
+		sidebarStyle: "mini",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	var dezThemeSet7 = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		primary: "color_2",
+		headerBg: "color_1",
+		navheaderBg: "color_2",
+		sidebarBg: "color_2",
+		sidebarStyle: "mini",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+	
+	var dezThemeSet8 = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		primary: "color_2",
+		headerBg: "color_14",
+		navheaderBg: "color_14",
+		sidebarBg: "color_2",
+		sidebarStyle: "full",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+		
+	function themeChange(theme, direction){
+		var themeSettings = {};
+		themeSettings = eval('dezThemeSet'+theme);
+		themeSettings.direction = direction;
+		dezSettingsOptions = themeSettings; /* For Screen Resize */
+		new dezSettings(themeSettings);
+		
+		setThemeInCookie(themeSettings);
+	}
+	
+	function setThemeInCookie(themeSettings)
+	{
+		//console.log(themeSettings);
+		jQuery.each(themeSettings, function(optionKey, optionValue) {
+			setCookie(optionKey,optionValue);
+		});
+	}
+	
+	function setThemeLogo() {
+		var logo = getCookie('logo_src');
+		
+		var logo2 = getCookie('logo_src2');
+		
+		if(logo != ''){
+			jQuery('.nav-header .logo-abbr').attr("src", logo);
+		}
+		
+		if(logo2 != ''){
+			jQuery('.nav-header .logo-compact, .nav-header .brand-title').attr("src", logo2);
+		}
+	}
+	
+	function setThemeOptionOnPage()
+	{
+		if(getCookie('version') != '')
+		{
+			jQuery.each(themeOptionArr, function(optionKey, optionValue) {
+				var optionData = getCookie(optionKey);
+				themeOptionArr[optionKey] = (optionData != '')?optionData:dezSettingsOptions[optionKey];
+			});
+			dezSettingsOptions = themeOptionArr;
+			new dezSettings(dezSettingsOptions);
+			
+			setThemeLogo();
+		}
+	}
+	
+	/*  set switcher option start  */
+	function getElementAttrs(el) {
+		return [].slice.call(el.attributes).map((attr) => {
+			return {
+				name: attr.name,
+				value: attr.value
+			}
+		});
+	}
+	
+	function handleSetThemeOption(item, index, arr) {
+		
+		var attrName = item.name.replace('data-','').replace('-','_');
+		
+		if(attrName === "sidebarbg" || attrName === "primary" || attrName === "headerbg" || attrName === "nav_headerbg" ){
+			if(item.value === "color_1"){
+				return false;
+			}
+			var attrNameColor = attrName.replace("bg","")
+			document.getElementById(attrNameColor+"_"+item.value).checked = true;
+		}else if(attrName === "navigationbarimg"){
+			document.getElementById("sidebar_img_"+item.value.split('sidebar-img/')[1].split('.')[0]).checked = true;
+		}else if(attrName === "sidebartext"){
+			document.getElementById("sidebar_text_"+item.value).checked = true;
+		}else if(attrName === "direction" || attrName === "nav_headerbg" || attrName === "headerbg"){
+			document.getElementById("theme_direction").value = item.value;	
+		}else if(attrName === "sidebar_style" || attrName === "sidebar_position" || attrName === "header_position" || attrName === "typography" || attrName === "theme_version" ){
+			if(item.value === "cairo" || item.value === "full" || item.value === "fixed"|| item.value === "light"){return false}
+			document.getElementById(attrName).value = item.value;				
+		}else if(attrName === "layout"){
+			if(item.value === "vertical"){return false}
+			document.getElementById("theme_layout").value = item.value;		
+		}
+		else if(attrName === "container"){
+			if(item.value === "wide"){return false}
+			document.getElementById("container_layout").value = item.value;
+		}
+		
+		$('.default-select').selectpicker('refresh');
+	}
+	/* / set switcher option end / */
+
+
+	jQuery(document).on('click', '.dz_theme_demo', function(){
+		setTimeout(() => {
+			var allAttrs = getElementAttrs(document.querySelector('body'));
+			allAttrs.forEach(handleSetThemeOption);
+		},1500);
+		var demoTheme = jQuery(this).data('theme');
+		themeChange(demoTheme, 'ltr');
+		$('.dz-demo-panel').removeClass('show');
+		jQuery('.main-css').attr('href','css/style.css');
+	});
+
+
+	jQuery(document).on('click', '.dz_theme_demo_rtl', function(){
+		var demoTheme = jQuery(this).data('theme');
+		themeChange(demoTheme, 'rtl');
+		$('.dz-demo-panel').removeClass('show');
+		jQuery('.main-css').attr('href','css/style-rtl.css');
+	});
+	
+	
+	jQuery(window).on('load', function(){
+		direction = (direction != undefined)?direction:'ltr';
+
+		if(getCookie('direction') == 'rtl'){
+			jQuery('.main-css').attr('href','css/style-rtl.css');
+		}
+
+		if(theme != undefined){
+			themeChange(theme, direction);
+		}else if(direction != undefined){
+			if(getCookie('version') == ''){	
+				themeChange(0, direction);
+			}
+		}
+		
+		setTimeout(() => {
+			var allAttrs = getElementAttrs(document.querySelector('body'));
+			allAttrs.forEach(handleSetThemeOption);
+		},1500);
+
+		/* Set Theme On Page From Cookie */
+		setThemeOptionOnPage();
+	});
+	
+	jQuery(window).on('resize', function(){
+		setThemeOptionOnPage();
+	});
+	
+})(jQuery);
---
+++ resources/views/login.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="robots" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Zenix - Crypto Admin Dashboard">
+    <meta property="og:title" content="Zenix - Crypto Admin Dashboard">
+    <meta property="og:description" content="Zenix - Crypto Admin Dashboard">
+    <meta property="og:image" content="https://zenix.dexignzone.com/xhtml/social-image.png">
+    <meta name="format-detection" content="telephone=no">
+    <title>Liza Gaming </title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon.png">
+    <link href="./vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
+    <link class="main-css" href="{{ url('assets/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="vh-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                    <div class="text-center mb-3">
+                                        <a href="{{url('/')}}" class="brand-logo">
+                                            <svg class="logo-abbr" width="50" height="50" viewBox="0 0 50 50"
+                                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <rect class="svg-logo-rect" width="50" height="50" rx="6"
+                                                    fill="#EB8153" />
+                                                <path class="svg-logo-path"
+                                                    d="M17.5158 25.8619L19.8088 25.2475L14.8746 11.1774C14.5189 9.84988 15.8701 9.0998 16.8205 9.75055L33.0924 22.2055C33.7045 22.5589 33.8512 24.0717 32.6444 24.3951L30.3514 25.0095L35.2856 39.0796C35.6973 40.1334 34.4431 41.2455 33.3397 40.5064L17.0678 28.0515C16.2057 27.2477 16.5504 26.1205 17.5158 25.8619ZM18.685 14.2955L22.2224 24.6007L29.4633 22.6605L18.685 14.2955ZM31.4751 35.9615L27.8171 25.6886L20.5762 27.6288L31.4751 35.9615Z"
+                                                    fill="white" />
+                                            </svg>
+                                            <h2 class="brand-title">Liza Gaming</h2>
+                                        </a>
+                                    </div>
+                                    <h4 class="text-center mb-4">Sign in your account</h4>
+                                    <form action="index.html">
+                                        <div class="form-group">
+                                            <label class="mb-1"><strong>Email</strong></label>
+                                            <input type="email" class="form-control" value="hello@example.com">
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="mb-1"><strong>Password</strong></label>
+                                            <input type="password" class="form-control" value="Password">
+                                        </div>
+                                        <div class="form-row d-flex justify-content-between mt-4 mb-2 flex-wrap">
+                                            <div class="form-group">
+                                                <div class="custom-control custom-checkbox ms-1">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="basic_checkbox_1">
+                                                    <label class="form-check-label" for="basic_checkbox_1">Remember my
+                                                        preference</label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <a href="page-forgot-password.html">Forgot Password?</a>
+                                            </div>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
+                                        </div>
+                                    </form>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    {{-- <script src="{{url('assets/js/global.min.js')}}"></script> --}}
+    <script src="{{ url('assets/js/bootstrap-select.min.js') }}"></script>
+    <script src="{{ url('assets/js/custom.js') }}"></script>
+    <script src="{{ url('assets/js/deznav-init.js') }}"></script>
+    <script src="{{ url('assets/js/main.js') }}"></script>
+    <script src="{{ url('assets/js/styleSwitcher.js') }}"></script>
+</body>
+
+</html>
