c1c2bd4255c51673b0c62193b8bff0629a1d8638
Hancie123 <hanciewanemphago@gmail.com>
2024-02-19 15:31:08 +0545

    note added

--- app/Livewire/NoteLivewire.php
+++ app/Livewire/NoteLivewire.php
@@ -8,9 +8,19 @@ use Livewire\Component;
 class NoteLivewire extends Component
 {
     public $notes;
+    public $word;
     public function render()
     {
-        $this->notes = Note::latest()->get();
+
+        if (!empty($this->word)) {
+            $this->notes = Note::where('title', 'like', '%' . $this->word . '%')
+            ->orWhere('note', 'like', '%' . $this->word . '%')->get();
+        } else {
+            // Fetch 5 random words when the search input is empty
+            $this->notes =
+            Note::latest()->get();
+        }
+
         return view('livewire.note-livewire');
     }
 }
