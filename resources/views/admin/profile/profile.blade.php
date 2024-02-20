9ccf60002acd36e1151ec35e0814854e0a5b4cb7
Hancie123 <hanciewanemphago@gmail.com>
2024-02-20 14:45:58 +0545

    customer added

--- resources/views/admin/profile/profile.blade.php
+++ resources/views/admin/profile/profile.blade.php
@@ -2,18 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
-    <meta name="robots" content="">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="description" content="Zenix - Crypto Admin Dashboard">
-    <meta property="og:title" content="Zenix - Crypto Admin Dashboard">
-    <meta property="og:description" content="Zenix - Crypto Admin Dashboard">
-    <meta property="og:image" content="https://zenix.dexignzone.com/xhtml/social-image.png">
-    <meta name="format-detection" content="telephone=no">
-    <title>Liza Gaming</title>
+
     @include('layouts.header')
 
 
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,18 @@
-<link rel="stylesheet" href="{{ url('assets/css/chartist.min.css') }}">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="robots" content="">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Zenix - Crypto Admin Dashboard">
+    <meta property="og:title" content="Zenix - Crypto Admin Dashboard">
+    <meta property="og:description" content="Zenix - Crypto Admin Dashboard">
+    <meta property="og:image" content="https://zenix.dexignzone.com/xhtml/social-image.png">
+    <meta name="format-detection" content="telephone=no">
+    <title>Liza Gaming</title>
+    <link rel="stylesheet" href="{{ url('assets/css/chartist.min.css') }}">
     <link href="{{ url('assets/css/owl.carousel.css') }}" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/img/favicon.png') }}">
     <link href="{{ url('assets/css/bootstrap-select.min.css') }}" rel="stylesheet">
     <link class="main-css" href="{{ url('assets/css/style.css') }}" rel="stylesheet">
-      <link rel="stylesheet" href="{{ url('assets/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{ url('assets/css/select2.min.css') }}">
